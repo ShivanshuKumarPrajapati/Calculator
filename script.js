56abe3f8294e84bd3dc034931984adbb896e7ed9
ShivanshuKumarPrajapati <shivanshu.rkt@gmail.com>
2021-10-14 09:02:49 +0530

    caluclator is able to perform basic operation

--- script.js
+++ script.js
@@ -1,53 +1,94 @@
 $(document).ready(function(){
     let t=0;
-    $('#zero').click(function(){
-        a = 0;
-        t = 1;
-        $("#screen").text(a);
-    });
-    $('#first').click(function () {
-        a = 1;
-        t = 1;
-        $("#screen").text(a);
-    });
-    $('#second').click(function () {
-        a = 2;
-        t = 1;
-        $("#screen").text(a);
-    });
-    $('#third').click(function () {
-        a = 3;
-        t = 1;
-        $("#screen").text(a);
-    });
-    $('#four').click(function () {
-        a = 4;
-        t = 1;
-        $("#screen").text(a);
-    });
-    $('#five').click(function () {
-        a = 5;
-        t = 1;
-        $("#screen").text(a);
-    });
-    $('#six').click(function () {
-        a = 6;
-        t = 1;
-        $("#screen").text(a);
-    });
-    $('#seven').click(function () {
-        a = 7;
-        t = 1;
-        $("#screen").text(a);
-    });
-    $('#eight').click(function () {
-        a = 8;
-        t = 1;
-        $("#screen").text(a);
-    });
-    $('#nine').click(function () {
-        a = 9;
-        t = 1;
-        $("#screen").text(a);
-    });
+    let res=0;
+    let buffer = "0";
+    let previousOperator;
+    function buttonClick(event)
+    {
+        var a=$(event.target);
+        var b=a.text();
+        if(isNaN(b))
+        operation(b);
+        else
+        numbers(b);
+        $('#screen').text(buffer);
+    }
+
+
+    //storing each  clicked digit in buffer
+    function numbers(digit)
+    {
+        if(buffer == '0')
+        buffer = digit;
+        else
+        {
+            buffer+=digit;
+        }
+        console.log(buffer);
+        $('#screen').text(buffer);
+    }
+
+    //handling the clicked operator
+    function operation(operator)
+    {
+        switch(operator)
+        {
+            case 'C':
+                buffer='0';
+                res=0;
+                $('#screen').text(buffer);
+                break;
+            case '=':
+                if(previousOperator === null)
+                alert('Invalid operation');
+                else
+                {
+                flushOperation(parseInt(buffer));
+                previousOperator=null;
+                buffer=res;
+                res=0;
+                }
+                break;
+            case '÷':
+            case '+':
+            case '×':
+            case '-':
+                calculation(operator);
+                break;
+        }
+    }
+    function calculation(symbol)
+    {
+        if(buffer === '0')
+        {
+        alert('Enter the value first');
+        return;
+        }
+            const intBuffer = parseInt(buffer)
+            console.log(intBuffer);
+            if(res===0)
+            res=intBuffer;
+            else
+            flushOperation(intBuffer);
+
+            previousOperator=symbol;
+            buffer='0';
+            console.log(res);
+        
+    }
+    function flushOperation(intBuffer)
+    {
+
+        if(previousOperator === '+')
+        res+=intBuffer;
+        else if (previousOperator === '-')
+        res-=intBuffer;
+        else if (previousOperator === '×')
+        res*=intBuffer;
+        else
+        res/=intBuffer;
+    }
+    $('.child-item').click(function(event){
+        buttonClick(event)
+    })
 })
